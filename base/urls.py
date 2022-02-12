e6e3050e901e9ed69c808be221b951db23eaff92
Mushfiqur719 <rahmanmushfiqur719@gmail.com>
2022-02-12 13:28:09 +0600

    2: user registraion

--- base/urls.py
+++ base/urls.py
@@ -3,10 +3,13 @@ from .import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
+
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutPage, name='logout'),
-    path('room/<str:pk>/', views.room, name='room'),
-    path('create-room/',views.createRoom, name='create-room'),
-    path('update-room/<str:pk>/',views.updateRoom, name='update-room'),
-    path('delete-room/<str:pk>/',views.deleteRoom, name='delete-room'),
+    path('register/', views.registerPage, name='register'),
+
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
+    path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
 ]
--- base/views.py
+++ base/views.py
@@ -5,10 +5,15 @@ from django.db.models import Q
 from django.contrib import messages
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login,logout
 
 def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -25,12 +30,26 @@ def loginPage(request):
         else:
             messages.error(request,'Username of Password does not match')
 
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 def logoutPage(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration')
+    return render(request, 'base/login_register.html',{'form':form})
 
 
 def home(request):
