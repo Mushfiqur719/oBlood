b68929a28bd2666c7c63303f0eff8c0baca8ca12
Mushfiqur719 <rahmanmushfiqur719@gmail.com>
2022-02-19 00:23:43 +0600

    3: Completed comment section

--- base/models.py
+++ base/models.py
@@ -13,8 +13,12 @@ class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL,null=True)
     description = models.TextField(null=True,blank=True)
+    participants = models.ManyToManyField(User,related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated','-created']
 
     def __str__(self):
         return self.name
--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from .forms import RoomForm
-from .models import Room,Topic
+from .models import Room,Topic,Message
 from django.http import HttpResponse
 from django.db.models import Q
 from django.contrib import messages
@@ -71,7 +71,20 @@ def home(request):
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room,'room_messages':room_messages,
+    'participants':participants}
     return render(request, 'base/room.html',context)
 
 @login_required(login_url='/login')
@@ -103,6 +116,7 @@ def updateRoom(request,pk):
     context = {'form':form}
     return render(request, 'base/room_form.html',context)
 
+@login_required(login_url='/login')
 def deleteRoom(request,pk):
     room = Room.objects.get(id=pk)
 
@@ -113,4 +127,17 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')
     context = {'obj' : room}
+    return render(request, 'base/delete.html',context)
+
+@login_required(login_url='/login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse("You are not allowed to delete someone elses post")
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    context = {'obj' : message}
     return render(request, 'base/delete.html',context)
