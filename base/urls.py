456a2297fdb3d8c6b664798ad3ab96247829f064
Mushfiqur719 <rahmanmushfiqur719@gmail.com>
2022-02-23 15:45:14 +0600

    8: Dynamic data after theme installation

--- base/urls.py
+++ base/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
     path('delete-message/<str:pk>/', views.deleteMessage, name='delete-message'),
+
+    
 ]
